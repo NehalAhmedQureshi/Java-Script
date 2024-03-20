4d58d4f2c957f9102d2df61c4170afe02c908a0c
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-20 22:36:36 +0500

    add first assigment on alert topic

---
+++ Assignments/3_PDF Assignment/01_alert/index.js
@@ -0,0 +1,8 @@
+alert("Error! Please enter a valid password.") ;
+
+alert("Welcome to JS Land... \n  Happy Coding!") ;
+
+alert("Welcome to JS Land.") ;
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs") ;
+
