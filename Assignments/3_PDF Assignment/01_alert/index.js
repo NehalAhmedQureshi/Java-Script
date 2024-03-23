2e54c7e60aec43ca52a70ed5670e69cc84447a78
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-23 08:50:59 +0500

    delete assignment 1 and 3

--- Assignments/3_PDF Assignment/01_alert/index.js
+++
@@ -1,8 +0,0 @@
-alert("Error! Please enter a valid password.") ;
-
-alert("Welcome to JS Land... \n  Happy Coding!") ;
-
-alert("Welcome to JS Land.") ;
-
-alert("Happy Coding! \n Prevent this page from creating additional dialogs") ;
-
