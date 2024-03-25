ec00edf671cdeba063a0b7a050e3c0219ad01c8d
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-25 23:37:09 +0500

    complete 6 chapter assignments

---
+++ Assignments/3_PDF Assignment/06_Math Expresion/index.js
@@ -0,0 +1,16 @@
+// *************************************** Question 1 *********************************
+let a = 10;
+console.log("The value of a is : " + a );
+
+// a = ++a;
+console.log("The value of ++a is : " + ++a);
+console.log("Now the value of a is" + a);
+
+console.log("The value of a++ is :" + a++);
+console.log("The value of a is :" + a);
+
+console.log("The value of --a is : " + --a);
+console.log("The value of a is : " + a);
+
+console.log("The value of a-- is : " + a--);
+console.log("The value of a is : " + a);
