f344e4e62d72c8aeb4cec7ef20ec3ca14120bc4d
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-25 23:51:43 +0500

    loops

---
+++ Classess/04_loop.js
@@ -0,0 +1,17 @@
+// for (initialization; condition; increment) {
+// 	loop_body
+// }
+
+// for (1; 2; 4) {
+//   3;
+// }
+
+// For Loop
+// AK code ko bar bar chalane ko istamal hota hy
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i, "Nehal");
+//   }
+
+for (let a = 0; a < 10; a++) {
+    console.log(a , "Nehal");
+}
