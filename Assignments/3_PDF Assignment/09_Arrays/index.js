6ed7be7891d7f5fc949b9b9f08ca64fae64abf8e
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-30 01:51:48 +0500

    chapter 9 assignment

---
+++ Assignments/3_PDF Assignment/09_Arrays/index.js
@@ -0,0 +1,10 @@
+//  ******************************** Question 1 *********************************
+
+let studentNames = []
+let string = ["Alphabets", "Math", "English", "Urdu"];
+let number = [1, 2, 3, 4, 5, 6];
+let boolean = [true, false];
+let mixed = ["String", 22, 34, true, false];
+let qualification = ["SSC", "HSC", "BCS", "BS", " BCOM", " MS", " M.Phil", "PhD"];
+
+console.log(qualification);
