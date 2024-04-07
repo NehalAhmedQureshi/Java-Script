4d3c529fa9338b97e1963fc1af213bdf50abc0a4
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-07 15:17:35 +0500

    complete 11 questions in chapter 9

--- Assignments/3_PDF Assignment/09_Arrays/index.js
+++ Assignments/3_PDF Assignment/09_Arrays/index.js
@@ -1,13 +1,69 @@
-//  ******************************** Question 1 *********************************
+//  ******************************** Question 2 *********************************
 
 let studentNames = []
+
+//  ******************************** Question 3 *********************************
 let string = ["Alphabets", "Math", "English", "Urdu"];
+
+//  ******************************** Question 4 *********************************
 let number = [1, 2, 3, 4, 5, 6];
+
+//  ******************************** Question 5 *********************************
 let boolean = [true, false];
+
+//  ******************************** Question 6 *********************************
 let mixed = ["String", 22, 34, true, false];
+
+//  ******************************** Question 7 *********************************
 let qualification = ["SSC", "HSC", "BCS", "BS", " BCOM", " MS", " M.Phil", "PhD"];
 
-console.log(qualification);
+// console.log(qualification);
+
+//  ******************************** Question 8 *********************************
+
+let studentOneName = "Nehal";
+let studentTwoName = "Shahid";
+let studentThreeName = "Zubair";
+
+let studentOneScore = 250;
+let studentTwoScore = 250;
+let studentThreeScore = 250;
+
+let totalScore = 500 ;
+
+// console.log(`Score of ${studentOneName} is ${totalScore-studentOneScore}.Percentage: ${(studentOneScore/totalScore)*100}%`);
+// console.log(`Score of ${studentTwoName} is ${totalScore-studentTwoScore}.Percentage: ${(studentTwoScore/totalScore)*100}%`);
+// console.log(`Score of ${studentThreeName} is ${totalScore-studentThreeScore}.Percentage: ${(studentThreeScore/totalScore)*100}%`);
+
+//  ******************************** Question 9 *********************************
+
+let color = ["Red","blue","Yellow","Green","Black"]
+// alert(color);
+
+// let addColor = prompt("Enter color name would you like to add at the end of the array")
+
+// color.push(addColor)
+// alert(color)
+
+// let addEndColor = prompt("Enter color name would you like to add at the start of the array")
+
+// color.unshift(addEndColor)
+// alert(color)
+console.log(color);
+
+color.push("Pink")
+color.unshift("Grey")
+
+console.log(color);
+
+color.pop()
+console.log(color);
+
+color.shift()
+console.log(color);
+
+color.map
+
 //  ******************************** Question 10 *********************************
 
 let studentScore =[223,344,123,343,];
