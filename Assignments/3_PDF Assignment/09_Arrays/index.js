05eb43fa16b96f83831c522359fe91e5abf3d596
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-07 15:15:57 +0500

    complete questin 11 in chap 9

--- Assignments/3_PDF Assignment/09_Arrays/index.js
+++ Assignments/3_PDF Assignment/09_Arrays/index.js
@@ -8,3 +8,33 @@ let mixed = ["String", 22, 34, true, false];
 let qualification = ["SSC", "HSC", "BCS", "BS", " BCOM", " MS", " M.Phil", "PhD"];
 
 console.log(qualification);
+//  ******************************** Question 10 *********************************
+
+let studentScore =[223,344,123,343,];
+
+// console.log(studentScore);
+
+function one (){
+    // studentScore.push(prompt("Enter Students Score: "))
+}
+one()
+
+console.log(studentScore);
+studentScore.sort()
+console.log(studentScore);
+
+//  ******************************** Question 11 *********************************
+const printCityName = document.querySelector("#citiesNames")
+const printSelectedCity = document.querySelector("#selectedCities")
+
+
+let cities = ['Karachi','Lahore','Islamabad','Hyderabad','Peshawar']
+
+let citiesName = (cities);
+
+let selectedCities =(cities.slice(1,4))
+
+// console.log(cities);
+
+printCityName.innerHTML = citiesName;
+printSelectedCity.innerHTML = selectedCities;
