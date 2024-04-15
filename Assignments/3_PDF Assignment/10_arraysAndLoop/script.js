97577089da28531cca911eabcaef8797396e61cc
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-15 11:29:16 +0500

    conplete all chap assignments

--- Assignments/3_PDF Assignment/10_arraysAndLoop/script.js
+++ Assignments/3_PDF Assignment/10_arraysAndLoop/script.js
@@ -4,27 +4,40 @@
 
 //  ******************************** Question 3 *********************************
 
-for (let i = 1 ; i <= 10; i++) {
-    // console.log(i);;
-}
+// for (let i = 1 ; i <= 10; i++) {
+//     // console.log(i);;
+// }
 
 //  ******************************** Question 4 *********************************
 
 // let table = prompt("Enter a number to show its multiplication table") ; 
 // let tableLength = prompt("Enter length multiplication table") ;
 
-for (let i = 0; i <= tableLength; i++) {
+// for (let i = 0; i <= tableLength; i++) {
     
-    const body = `${table} X ${i} = ${ table*i }`
-    // console.log(body);
+//     const body = `${table} X ${i} = ${ table*i }`
+//     // console.log(body);
     
-}
+// }
 
 
 //  ******************************** Question 5 *********************************
 
 //  ******************************** Question 6 *********************************
+// let counting = 1;
 
+for (let counting = 1 ; counting <= 15 ; counting++) {
+    console.log(counting);
+}
+for (let reverce_counting = 10; reverce_counting > 0 ; reverce_counting--) {
+    console.log(reverce_counting);   
+}
+for (let odd_counting = 0; odd_counting < 15; odd_counting += 2) {
+    console.log(odd_counting);   
+}
+for (let series = 1; series < 15; series++) {
+    console.log(`${series}k`);    
+}
 //  ******************************** Question 7 *********************************
 
 //  ******************************** Question 8 *********************************
@@ -32,3 +45,6 @@ for (let i = 0; i <= tableLength; i++) {
 //  ******************************** Question 9 *********************************
 
 //  ******************************** Question 10 *********************************
+for (let i = 0; i <= 100; i += 5) {
+    console.log(i);
+}
