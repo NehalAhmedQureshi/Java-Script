bdabd1d21f932418f4cd77dbd47c2a7f1b9cafd3
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-05-02 12:12:07 +0500

    different types of functions

---
+++ Classess/10_functions.js
@@ -0,0 +1,9 @@
+
+// ---------------------------------------- functions -------------------------------------
+
+function myfunction(num1,num2) {
+      let a = num1 + num2
+
+      return 50 ;
+}
+ console.log(myfunction(5,6));
---
+++ Classess/11_functionParameters.js
@@ -0,0 +1,23 @@
+
+let array = [3,5,6,7,8]
+
+console.log(array);
+console.log(array.length);
+//  ---------------------------- length is not a function -------------------------
+
+function average(myArray) {
+
+      let sum = 0 
+
+      for (let i = 0; i < myArray.length; i++) {
+
+            sum += myArray[i]
+            
+      }
+
+      return sum / myArray.length
+
+}
+
+console.log(average([2,4,5,6,7,6]));
+console.log(average([2,4,5,6,7,6,10 , 40,100,1000]));
